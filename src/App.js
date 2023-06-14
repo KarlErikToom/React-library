1b0e80f03a89c1ce30eb3ab405f7f35c4f12fbe8
Karl <toom.karl42@gmail.com>
2023-06-14 14:27:11 +0300

    books

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import "./App.css";
 import Nav from "./components/Nav";
-import Landing from './components/Landing'
-import Highlights from "./components/Highlights";
-import Featured from "./components/Featured";
-
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from "./data";
 
 function App() {
-  return <div className="App">
-    <Nav />
-    <Landing />
-    <Highlights />
-    <Featured />
-  </div>;
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" render={() =><Books books={books} />} />
+
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
