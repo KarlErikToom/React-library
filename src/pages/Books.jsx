b2fb12c1945c13bf653a044d07274750c6f4fb7e
Karl <toom.karl42@gmail.com>
2023-06-14 14:23:49 +0300

    started books page

---
+++ src/pages/Books.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Book from "../components/ui/Book";
+import { useState } from "react";
+
+function Books( {books: initialBooks}) {
+    const [books, setBooks]= useState(initialBooks)
+
+    function filterBooks(){
+        console.log("filter")
+    }
+  return (
+    <div id="books__body">
+      <main id="books__main">
+        <section>
+          <div className="books__container">
+            <div className="row">
+              <div className="books__header">
+                <h2 className="section__title books__header--title">
+                  All Books
+                </h2>
+                <select id="filter" defaultValue="DEFAULT" onChange={filterBooks}>
+                  <option value="DEFAULT" disabled>
+                    Sort
+                  </option>
+                  <option value="LOW_TO_HIGH">Price, Low to High</option>
+                  <option value="HIGH_TO_LOW">Price, High to Low</option>
+                  <option value="RATING">Rating</option>
+                </select>
+              </div>
+              <div className="books">
+                {books.map((book) => (
+                  <Book book={book} key={book.id} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default Books;
