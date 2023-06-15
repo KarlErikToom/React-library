c16e66a81c4c02c54051829327f8a45666a3c9ac
Karl <toom.karl42@gmail.com>
2023-06-15 14:00:45 +0300

    added cart page html and css

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import Books from "./pages/Books";
 import { books } from "./data";
 import Bookinfo from "./pages/Bookinfo";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/books" exact render={() =><Books books={books} />} />
         <Route path="/books/:id"  render={() => <Bookinfo books={books} />} />
+        <Route path="/cart"  render={() => <Cart books={books} />} />
 
         <Footer />
       </div>
