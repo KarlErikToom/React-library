5cbf84d6a9505f75e2f38af9b844b97dd51a8bf1
Karl <toom.karl42@gmail.com>
2023-06-15 16:49:39 +0300

    added skeleton loading state and basically finished the website

--- src/App.js
+++ src/App.js
@@ -36,6 +36,14 @@ function App() {
     setCart(cart.filter(book => book.id !== item.id))
   }
 
+  function numberOfItems(){
+    let counter = 0;
+    cart.forEach(item =>{
+      counter += item.quantity
+    })
+    return counter
+  }
+
   useEffect(() => {
     console.log(cart);
   }, [cart]);
@@ -47,7 +55,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Nav />
+        <Nav numberOfItems={numberOfItems()} />
         <Route path="/" exact component={Home} />
         <Route path="/books" exact render={() => <Books books={books} />} />
         <Route
